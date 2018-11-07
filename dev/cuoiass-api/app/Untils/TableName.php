88861882628c4df03fb5504c617a1d721fac4e8d
Hồng Duy <hongduyabc@gmail.com>
2018-11-07 22:09:14 +0700

    Add Common table name

---
+++ dev/cuoiass-api/app/Untils/TableName.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Until;
+
+
+class TableName
+{
+    public static $n = [
+        'TBL_ACCOUNTS' => 'accounts',
+        'TBL_TBL_ADMINS' => 'admins',
+        'TBL_ANALYSIS' => 'analysis',
+        'TBL_BOOKED_CUSTOMIZE_FIELDS' => 'booked_customize_fields',
+        'TBL_BOOKED_FOODS' => 'booked_foods',
+        'TBL_BOOKED_HONEY_MOONS' => 'booked_honey_moons',
+        'TBL_BOOKED_OPTIONS' => 'booked_options',
+        'TBL_BOOKINGS' => 'bookings',
+        'TBL_CREDITS' => 'credits',
+        'TBL_CUSTOMERS' => 'customers',
+        'TBL_CUSTOMIZE_FIELDS' => 'customize_fields',
+        'TBL_DAILY_REPORTS' => 'daily_reports',
+        'TBL_DRINKS' => 'drinks',
+        'TBL_FEES' => 'fees',
+        'TBL_FOODS' => 'foods',
+        'TBL_IMAGES' => 'images',
+        'TBL_MASTER_SERVICES' => 'master_services',
+        'TBL_MENUS' => 'menus',
+        'TBL_MONTHLY_REPORTS' => 'monthly_reports',
+        'TBL_OPTIONS' => 'options',
+        'TBL_PACKAGE_PRODUCTS' => 'package_products',
+        'TBL_PACKAGES' => 'packages',
+        'TBL_PLANS' => 'plans',
+        'TBL_PRODUCTS' => 'products',
+        'TBL_PROMOTION_PRODUCTS' => 'promotion_products',
+        'TBL_PROMOTIONS' => 'promotions',
+        'TBL_REVIEWS' => 'reviews',
+        'TBL_ROLES' => 'roles',
+        'TBL_SCHEDULE_PHOTOS' => 'schedule_photos',
+        'TBL_STAFFS' => 'staffs',
+        'TBL_TRAVEL_PRODUCTS' => 'travel_products',
+        'TBL_VENDOR_SERVICES' => 'vendor_services',
+        'TBL_VENDORS' => 'vendors',
+        'TBL_WEEKLY_REPORTS' => 'weekly_reports'
+    ];
+}
